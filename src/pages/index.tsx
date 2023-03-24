ed2418a81221545b42af9bb6cf2e00a4417eea71
Archie <archie@veed.io>
2023-03-24 18:27:02 +0000

    use sizes prop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
+import NextImage, { ImageProps as NextImageProps } from "next/image";
 import calendar from "@/../public/calendar.png";
+import clsx from "clsx";
 
 export default function Home() {
   return (
@@ -11,8 +12,59 @@ export default function Home() {
         placeholder="blur"
         width={1078}
         height={522}
+        sizes={{
+          xs: 328,
+          sm: 608,
+          md: 736,
+          lg: 992,
+          xl: 1078,
+        }}
+        quality={100}
         priority
       />
     </div>
   );
 }
+
+type SizeOptions = {
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+};
+
+export type ImageProps = Omit<
+  NextImageProps,
+  "src" | "width" | "height" | "sizes"
+> &
+  ({ src: NextImageProps["src"] } | { hash: string; ext: string }) & {
+    width: number;
+    height: number;
+    sizes?: SizeOptions;
+  };
+
+export const Image = (props: ImageProps) => {
+  const { src, hash, ext, width, height, sizes, ...rest } = props as any;
+  const imageSizes = sizes && getSizes(sizes, width);
+  return (
+    <NextImage
+      {...rest}
+      src={src}
+      sizes={imageSizes}
+      height={height}
+      width={width}
+    />
+  );
+};
+
+const getSizes = (opts: SizeOptions, width: number): string => {
+  return clsx(
+    `(min-width: 1536px) ${width}px, `,
+    `(min-width: 1280px) ${opts.xl}px,`,
+    `(min-width: 1024px) ${opts.lg}px,`,
+    `(min-width: 768px) ${opts.md}px,`,
+    `(min-width: 640px) ${opts.sm}px,`,
+    `${opts.xs}px` // 360px
+  );
+};
